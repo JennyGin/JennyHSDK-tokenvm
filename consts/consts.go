2c23692991dc7deeccfa932c70ec65f7cc44a9d4
JennyGin <ginikaodimegwu1@gmail.com>
2024-06-13 14:52:36 +0000

    Jayar

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "JayarHRP"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Jayar"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JR"
 )
 
 var ID ids.ID
